0342cd5894957c793091417551d387f7e79f63e2
azat <azat.uspanov@bk.ru>
2023-04-05 11:05:23 +0600

    SecondActivity has placed a date picker and an OK button. When you click on OK, the date is transmitted. Displayed the date in MainActivity.

--- app/src/main/java/com/example/lab4_mobile/SecondActivity.kt
+++ app/src/main/java/com/example/lab4_mobile/SecondActivity.kt
@@ -1,8 +1,12 @@
 package com.example.lab4_mobile
-
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
 import android.widget.TextView
+import java.util.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +15,16 @@ class SecondActivity : AppCompatActivity() {
         val name = intent.getStringExtra("name")
         val textView = findViewById<TextView>(R.id.textView)
         textView.text = "Привет, $name!"
+        val datePicker = findViewById<DatePicker>(R.id.datePicker)
+        val button = findViewById<Button>(R.id.buttonOk)
+
+        button.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            calendar.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)
+            val intent = Intent()
+            intent.putExtra("date", calendar.timeInMillis)
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
     }
 }
